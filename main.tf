ccf0f003340762cdce0393ae66fd99584067a15c
gvsiva2008 <95896898+gvsiva2008@users.noreply.github.com>
2022-01-27 09:15:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key =  "AKIATRQCHRRYHDQEGZVK"
-  secret_key = "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"
+
 }
 resource "aws_instance" "web" {
   ami           = "ami-04505e74c0741db8d"
