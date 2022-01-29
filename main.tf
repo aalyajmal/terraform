d98039000ae2627db0625faea593788d54335716
aalyajmal <82930770+aalyajmal@users.noreply.github.com>
2022-01-29 16:20:27 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,6 +10,14 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIATRQCHRRYDMN5TKAG"
+  secret_key = "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"
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
 
 }
 resource "aws_instance" "web" {
@@ -17,7 +25,8 @@ resource "aws_instance" "web" {
   instance_type = "t2.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "terraform"
   }
 }
+
 
